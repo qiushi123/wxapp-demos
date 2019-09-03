8ba48bafa08fcd592755084f36e362003b72188a
qiushi123 <2501902696@qq.com>
2019-09-03 10:33:09 +0800

    picker两级联动

--- cloud/getPhone/index.js
+++ cloud/getPhone/index.js
@@ -1,16 +1,36 @@
 // 云函数入口文件
+
 const cloud = require('wx-server-sdk')
+
+const requestpromise = require('request-promise');
+
+var WXBizDataCrypt = require('./RdWXBizDataCrypt') // 用于手机号解密
 
 cloud.init()
 
 // 云函数入口函数
+
 exports.main = async (event, context) => {
+
+  // 获取 WX Context (微信调用上下文)，包括 OPENID、APPID、及 UNIONID（需满足 UNIONID 获取条件）
   const wxContext = cloud.getWXContext()
 
+  const AccessToken_options = {
+    method: 'GET',
+    url: 'https://api.weixin.qq.com/sns/jscode2session',
+
+    qs: {
+      appid: wxContext.APPID,
+      secret: wxContext.UNIONID, // 微信开发后台可生成，唯有微信认证过的国内主体才可有
+      grant_type: 'authorization_code',
+      js_code: event.sessionCode // 小程序中获取过来的
+    },
+    json: true
+  };
+  const resultValue = await requestpromise(AccessToken_options);
+  const pc = new WXBizDataCrypt(appid, resultValue.session_key) // -解密第一步
+  const data = pc.decryptData(event.encryptedData, event.iv) // 解密第二步
   return {
-    event,
-    openid: wxContext.OPENID,
-    appid: wxContext.APPID,
-    unionid: wxContext.UNIONID,
+    data
   }
 }
--- pages/button/button.js
+++ pages/button/button.js
@@ -8,8 +8,40 @@ Page({
 
   },
   //获取用户手机号
-  getPhoneNumber(event) {
-    console.log(event)
+  getPhoneNumber(e) {
+    var that = this;
+    if (!e.detail.errMsg || e.detail.errMsg != "getPhoneNumber:ok") {
+      wx.showModal({
+        content: '不能获取手机号码',
+        showCancel: false
+      })
+      return;
+    }
+    wx.showLoading({
+      title: '获取手机号中...',
+    })
+    wx.cloud.callFunction({
+      name: 'getToken',  // 对应云函数名
+      data: {
+        encryptedData: e.detail.encryptedData,
+        iv: e.detail.iv,
+        sessionCode: app.globalData.sessionCode    // 这个通过wx.login获取，去了解一下就知道。这不多描述
+      },
+      success: res => {
+        wx.hideLoading()
+        // 成功拿到手机号，跳转首页
+        wx.switchTab({
+          url: '../index/index' // 这里是要跳转的路径
+        })
+      },
+      fail: err => {
+        console.error(err);
+        wx.showToast({
+          title: '获取手机号失败',
+          icon: 'none'
+        })
+      }
+    })
   }
 
 
